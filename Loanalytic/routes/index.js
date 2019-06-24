bb7019839326c2e29f5063ac4b9f0071349f3ead
Gangadhar Adusumalli <gangadharadusumalli1993@gmail.com>
2019-06-24 11:20:34 -0500

    add routes folder

---
+++ Loanalytic/routes/index.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index.ejs', { title: 'Login' });
+});
+
+
+
+module.exports = router;
