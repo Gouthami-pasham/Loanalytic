07507694efb6cafe37eaf4f9dbb7919c9e69e821
Vamsee Krishna Gangapatnam <crvamsee@gmail.com>
2019-06-26 11:10:01 -0500

    update mailer.js

--- Codes/mailer.js
+++ Codes/mailer.js
@@ -3,7 +3,6 @@ var nodemailer = require('nodemailer');
 var emailTemplates = require('email-templates');
 var sendMailTransport = require('nodemailer-smtp-transport');
  
-
 module.exports = {
   _template: null, 
   _transport: null,
