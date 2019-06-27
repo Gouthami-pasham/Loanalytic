c9495ddb95b561264f1484ec2257cfe03ac13bbd
keerthi sree <keerthisree9595@gmail.com>
2019-06-26 21:58:43 -0500

    updated adminhome.js

--- Codes/routes/adminhome.js
+++ Codes/routes/adminhome.js
@@ -1,3 +1,8 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
 router.get('/adminhome', function (req, res) {
     res.render('adminhome.ejs', { title: 'Login' });
 });
+module.exports = router;
