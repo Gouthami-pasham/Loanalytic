616ff6934f640f3a92ef4c8da0c3896895325e2e
Himabindu Poshala <himabindu.poshala23@gmail.com>
2019-09-29 21:00:41 -0500

    Added view_edit_profile.js

---
+++ Codes/controllers/view_edit_profile.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
+var registration = require('../models/registration');
+var mailer = require('../nodemailer');
+
+router.get('/', function (req, res) {
+    res.render('view_edit_profile.ejs', { title: 'view/edit profile' });
+});
