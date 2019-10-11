bb8fe3ce7a2b48d9fc876e5334394e9f468c4181
Gangadhar Adusumalli <gangadharadusumalli1993@gmail.com>
2019-10-10 23:47:30 -0500

    add new js pages

---
+++ Codes/public/js/adminviewapplication.js
@@ -0,0 +1,119 @@
+(function() {
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    
+    //   var scope = angular.element('[ng-controller=viewApplications]').scope()
+    //   scope.getTotalApplication();
+    }, false);
+   
+  })();
+//ng-init="getTotalApplication()"
+var app = angular.module('myApp', []);
+app.controller('viewApplications', function($scope, $http) {
+    $scope.totalApplication = [];
+    $scope.onApprove = function(application){
+        $scope.approveApplication = application;
+        $('#approveModal').modal('show');
+    }
+
+    $scope.onReject = function(application){
+        $scope.rejectApplication = application;
+        $('#rejectModal').modal('show');
+    }
+    $scope.getTotalApplication = function(){
+        var data = {
+            test: "test"
+        };
+        var config = {
+            headers : {
+                'Content-Type': 'application/json'
+            }
+        }
+        $http.get('http://localhost:3000/adminviewapplication/getTotal').then(function(response) {
+              // This function handles succes
+              console.log(response);
+              $scope.totalApplication = response.data;
+        
+    }, function(response) {
+
+        // this function handles error
+        console.log(response);
+    });
+}
+$scope.updateApplicationStatus = function(status){
+    var applicationData = {}; 
+    var src = "";
+    if(status == "Approved"){
+        applicationData = $scope.totalApplication[$scope.approveApplication]
+        src = "approve.png"
+
+      }
+      else{
+        applicationData = $scope.totalApplication[$scope.rejectApplication]
+        src = "rejected.jpg"
+      }
+    var data = {
+        status: status,
+        applicationId:applicationData.Application_id
+    };
+    var config = {
+        headers : {
+            'Content-Type': 'application/json'
+        }
+    }
+    $http.post('http://localhost:3000/adminviewapplication/updateStatus',data,config).then(function(response) {
+          // This function handles succes
+          console.log(response);
+          $scope.approveApplication = "";
+          $scope.rejectApplication = ""
+
+          var data = {
+            "subject": "Application Status",
+            "text": '<img src="cid:unique@kreata.ee" width="600px" height="500px" /> <br><h1 style="color:#008f95;">Welcome to Loanalytic</h1>',
+            "email": applicationData.Email,
+            "src":src
+        }
+          $http.post('http://localhost:3000/register/sendEmail', data, config).then(function(response) {
+            // This function handles succes
+            console.log(response);
+            if(response.status == 200 && response.statusText == "OK"){
+                if(status == "Approved"){
+                    $('#approveModal').modal('hide');
+                    $('#approveSuccessModal').modal('show');
+                  }
+                  else{
+                    $('#rejectModal').modal('hide');
+                    $('#rejectSuccessModal').modal('show');
+                  }
+            }
+            
+        }, function(response) {
+
+            // this function handles error
+            console.log(response);
+        });
+         
+          
+    
+}, function(response) {
+
+    // this function handles error
+    console.log(response);
+});
+}
+});
+
+$('#approveSuccessModal').on('hidden.bs.modal', function (e) {
+    var scope = angular.element('[ng-controller=viewApplications]').scope()
+    scope.getTotalApplication();
+});
+
+$('#rejectSuccessModal').on('hidden.bs.modal', function (e) {
+    var scope = angular.element('[ng-controller=viewApplications]').scope()
+    scope.getTotalApplication();
+});
+
+function updateStatus(applicationStatus){
+    var scope = angular.element('[ng-controller=viewApplications]').scope()
+    scope.updateApplicationStatus(applicationStatus);
+}
--- Codes/public/js/loanapplication.js
+++ Codes/public/js/loanapplication.js
@@ -1,12 +1,23 @@
+(function() {
+    
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    validate();
+  }, false);
+ 
+})();
+var app = angular.module('myApp', []);
 function validate(){
     // Loop over them and prevent submission
-    var form = $('#userRegsitration')[0];
+    var form = $('#loanApplication')[0];
     var scope = angular.element('[ng-controller=formCtrl]').scope()
     form.addEventListener('submit', scope.submitForm);
+
+    
   }
 
-  function formatDate(date) {
-    var d = new Date(date),
+  function formatDate() {
+    var d = new Date(),
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
         year = d.getFullYear();
@@ -16,88 +27,187 @@ function validate(){
 
     return [year, month, day].join('-');
 }
+
+
+
+
+function uploadFile(input){
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#blah')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  var fd = new FormData();
+  //Take the first selected file
+  fd.append("file", input.files[0]);
+
+  $http.post("http://localhost:3000/loanapplication/uploadDocument", fd, {
+      withCredentials: true,
+      headers: {'Content-Type': undefined },
+      transformRequest: angular.identity
+  }).then(function (response) {
+    console.log(response);
+  },
+  function(response){
+    console.log(response);
+  });
+}
+}
   var app = angular.module('myApp', []);
   app.controller('formCtrl', function($scope,$http) {
-    $scope.user = {
+    $scope.loanApplication = {
+      applicationId :"",
+      User_id:0,
       firstName: "", 
       lastName: "",
       phone:"",
       email:"",
-      dateofbirth:"",
+      loanAmount:"",
+      propertyTax:"",
+      downPayment:"",
+      loanTerm:"",
+      interestType:"",
       ssn:"",
-      employeestatus:"",
-      password:"",
-      confirmpassword:"",
+      gender:"",
+      loanType:"",
+      income:"",
+      applicationDate:formatDate(),
+      status:"In Progress",
+      creditScore:""
     };
     $scope.reset = function() {
      
     };
 
-    $scope.validateDate = function($event){
-      var birthday = event.currentTarget.valueAsDate;
-        var ageDifMs = Date.now() - birthday.getTime();
-        var ageDate = new Date(ageDifMs); // miliseconds from epoch
-        var age =  Math.abs(ageDate.getUTCFullYear() - 1970);
-       if(age < 21){
-          $('#date-failed').show();
-       }
-       else{
-        $('#date-failed').hide();
-       }
-       
+   
+
+    $scope.getApplicationId = function($event){
+      var toDate = new Date();
+      var month = toDate.getMonth()+1;
+      var day = toDate.getDate();
+      var year = toDate.getFullYear();
+      if (month.length < 2) month = '0' + month;
+      if (day.length < 2) day = '0' + day;
+      return year+""+month+""+day+""+toDate.getHours()+""+toDate.getMinutes()+""+ toDate.getSeconds();    
+    }
+
+    $scope.uploadDocument = function(input){
+       /* var data = {
+          hello : "test",
+          fileName : files[0].name,
+          fileType : files[0].type,
+          file: files[0]
+        }
+        var config = {
+          headers : {
+              'Content-Type': 'application/json'
+          }
+        }
+      $http.post('http://localhost:3000/loanapplication/uploadDocument', data, config).then(function(response) {
+            // This function handles succes
+            console.log(response);
+            if(response.status == 200 && response.statusText == "OK"){
+              
+            }
+            
+        }, function(response) {
+
+            // this function handles error
+            console.log(response)
+        })*/
+      var fd = new FormData();
+      fd.append('file',input.files);
+      
+      $http.post("http://localhost:3000/loanapplication/uploadDocument", fd, {
+          headers: {'Content-Type': undefined },
+          transformRequest: angular.identity
+      }).then(function (response) {
+        console.log(response);
+      },
+      function(response){
+        console.log(response);
+      });
+
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+  var formData = document.getElementById('loanApplication');
+  var dataSet = 12;
+    
     }
     
     $scope.submitForm = function(event){
-      var form = $('#userRegsitration')[0];
+      var form = $('#loanApplication')[0];
+     //var data =  $('#loanApplication').serialize();
       if (form.checkValidity() === false) {
         event.preventDefault();
         event.stopPropagation();
       }
       form.classList.add('was-validated'); 
-      if($scope.user.password != "" && $scope.user.confirmpassword != ""){
-        if($scope.user.password == $scope.user.confirmpassword){
-          $('#password-success').show();
-          $('#password-failed').hide();
-        }
-        else{
-          $('#password-success').hide();
-          $('#password-failed').show();
-          return;
-        }
-      }
+      
       if (form.checkValidity()) {
-        $('#date-failed').hide();
         var config = {
           headers : {
               'Content-Type': 'application/json'
           }
-      }
-      
-      $scope.user.dateofbirth = new Date($scope.user.dateofbirth); 
-      var data = $scope.user;
-      data.dateofbirth = formatDate($scope.user.dateofbirth);
-      data.ssn = parseInt($scope.user.ssn);
-        $http.post('http://localhost:3000/register/createuser', data, config).then(function (response) {
+        }
+     
+      var data = $scope.loanApplication;
+      data.ssn = parseInt($scope.loanApplication.ssn);
+      data.applicationId = parseInt($scope.getApplicationId());
+        $http.post('http://localhost:3000/loanapplication/saveApplication', data, config).then(function (response) {
           // This function handles succes
           console.log(response); 
           var data = {
-            "subject": "User Registration",
-            "text": '<img src="cid:unique@kreata.ee" width="600px" height="500px" /> <br><h1 style="color:#008f95;">Welcome to Loanalytic</h1>',
-            "email": $scope.user.email,
+            "subject": "Loan Application",
+            "text": '<img src="cid:unique@kreata.ee" width="600px" height="500px" /> <br><h1 style="color:#008f95;">Your Loan Application created sucessfully</h1>',
+            "email": $scope.loanApplication.email,
             "src":"register.png"
         }
-        $http.post('http://localhost:3000/register/sendEmail', data, config).then(function(response) {
+
+        var form_data = new FormData();
+        
+        /*$http.post('http://localhost:3000/loanapplication/createDocument', data, config).then(function(response) {
             // This function handles succes
+            
+
             console.log(response);
             if(response.status == 200 && response.statusText == "OK"){
-              $('#registerModal').modal('show');
+              
             }
             
         }, function(response) {
 
             // this function handles error
             console.log(response);
-        });
+        });*/
+        /*$http.post('http://localhost:3000/loanapplication/sendEmail', data, config).then(function(response) {
+            // This function handles succes
+            console.log(response);
+            if(response.status == 200 && response.statusText == "OK"){
+              
+            }
+            
+        }, function(response) {
+
+            // this function handles error
+            console.log(response);
+        });*/
           
           }, function (response) {
           
@@ -108,6 +218,6 @@ function validate(){
     }
   });
 
-  $('#registerModal').on('hidden.bs.modal', function (e) {
+ /*$('#registerModal').on('hidden.bs.modal', function (e) {
     location.href = '/';
-});
+});*/
