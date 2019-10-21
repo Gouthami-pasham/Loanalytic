6324d5c99b3b760b611d161762b37e74838dad3a
Vamsee Krishna Gangapatnam <s533908@nwmissouri.edu>
2019-10-20 23:03:02 -0500

    Add changes

--- Codes/testmailer.js
+++ Codes/testmailer.js
@@ -19,3 +19,35 @@ transporter.sendMail(mailOptions, function(error, info){
     }
     console.log('Message sent: ' + info.response);
 });
+
+
+
+/*
+function sendEmail ( _name, _email, _subject, _message) {
+    mandrill('/messages/send', {
+        message: {
+            to: [{email: _email , name: _name}],
+            from_email: 'noreply@yourdomain.com',
+            subject: _subject,
+            text: _message
+        }
+    }, function(error, response){
+        if (error) console.log( error );
+        else console.log(response);
+    });
+}
+
+// define your own email api which points to your server.
+
+app.post( '/api/sendemail/', function(req, res){
+
+    var _name = req.body.name;
+    var _email = req.body.email;
+    var _subject = req.body.subject;
+    var _messsage = req.body.message;
+
+    //implement your spam protection or checks. 
+
+    sendEmail ( _name, _email, _subject, _message );
+
+*/
